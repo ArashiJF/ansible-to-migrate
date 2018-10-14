b3c99ae77536858978f7842ad322eef9760d20e9
ArashiJF <juanfmf2209@hotmail.com>
2018-10-13 23:53:51 -0500

    Added add_host.sh to automatize adding the hosts Create_docker had the add_host command but that would clutter the hosts file so it was deleted dockerfile was edited to install ufw and allow incoming and outgoing traffic by default clean_all was edited to add the hosts files, but if that doesnt work, we will need to delete the keys by hand copy_code.yml was edited to send the code to another folder because it wasnt copying to an empty directory install_httpd commented the ufw rule set to avoid conflicts for at least getting all things to install for now Same with db/tasks/main.yml ufw is commented to avoid issues to get everything installed not safe tho

---
+++ add_host.sh
@@ -0,0 +1,2 @@
+echo "añadiendo a hosts"
+echo "127.0.0.1 web3 web2" | sudo tee -a /etc/hosts
--- clean_all.sh
+++ clean_all.sh
@@ -11,22 +11,20 @@ echo
 echo "----------------------"
 echo "Eliminando configuración residual en ssh"
 
-sed -i '' "/web3/d" $HOME/.ssh/known_hosts
-#ssh-keyscan -H web3 -p 2221 >> $HOME/.ssh/known_hosts
+sed -i '' "/web3" $HOME/.ssh/known_hosts
+ssh-keyscan -H web3 -p 2221 >> $HOME/.ssh/known_hosts
 
-sed -i '' "/web2/d" $HOME/.ssh/known_hosts
-#ssh-keyscan -H web2 -p 2222 >> $HOME/.ssh/known_hosts
+sed -i '' "/web2" $HOME/.ssh/known_hosts
+ssh-keyscan -H web2 -p 2222 >> $HOME/.ssh/known_hosts
 
 echo "Limpieza lista."
 echo
 echo "----------------------"
 echo "Recreando dockers"
-./create_dockers.sh server:16.04
+./create_docker.sh server:16.04
 
-ssh -o StrictHostKeyChecking=no root@server01 -p 2221 -i key.private hostname
-ssh -o StrictHostKeyChecking=no root@server02 -p 2222 -i key.private hostname
-ssh -o StrictHostKeyChecking=no root@server03 -p 2223 -i key.private hostname
+ssh -o StrictHostKeyChecking=no root@web3 -p 2221 -i key.private hostname
+ssh -o StrictHostKeyChecking=no root@web2 -p 2222 -i key.private hostname
 echo "Adicionadas las llaves ssh."
 echo
 echo "----------------------"
-echo "Sigue con ansible001"
